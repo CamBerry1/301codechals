869743ec71877e3990e95a9a860d04697580376e
Cameron Berry <cameronberry@Camerons-MacBook-Pro.local>
2023-05-06 17:59:10 -0700

    Beginning challenge

---
+++ codechal05.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Script: Ops 301 Code Challenge 05
+# Author: Cameron Berry
+# Date of last revision: 5/6/2023
+# Purpose: Print to the screen the file size of the log files before compression
+#          Compress the contents of the log files listed below to a backup directory
+#               /var/log/syslog
+#               /var/log/wtmp
+#          The file name should contain a time stamp with the following format -YYYYMMDDHHMMSS
+#               Example: /var/log/backups/syslog-20220928081457.zip
+#          Clear the contents of the log file
+#          Print to screen the file size of the compressed file
+#          Compare the size of the compressed files to the size of the original log files
+
+# Delcare variables
+
+
+
+# Declare functions
+
+
+
+# Main
+
+
+
+# End
