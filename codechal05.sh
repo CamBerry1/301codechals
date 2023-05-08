ba3f1de35cf49e103112f3fa29530a33a69e896e
CamBerry1 <cberry2008@gmail.com>
2023-05-07 18:15:33 -0700

    Code Challenge 05

--- codechal05.sh
+++ codechal05.sh
@@ -13,9 +13,12 @@
 #          Print to screen the file size of the compressed file
 #          Compare the size of the compressed files to the size of the original log files
 
-# Delcare variables
+#!/bin/bash
 
+# Declare variables
 
+datetime=`date +%y%m%d%H%M%S`
+backupfile="$HOME/Documents/backups/syslog_wtmp_$datetime.txt"
 
 # Declare functions
 
@@ -23,6 +26,33 @@
 
 # Main
 
-
+#make the backup directory in 'Documents'
+mkdir ~/Documents/backups
+#create the file to copy logs into
+touch $backupfile
+#write syslog contents and append wtmp contents to backupfile
+cat /var/log/syslog > $backupfile
+cat /var/log/wtmp >> $backupfile
+#read full filesize
+filesize=$(wc -c $backupfile | awk '{print $1}')
+echo "Syslog and Wtmp are currently $filesize bytes"
+read -p "Press Enter to clear logs and compress backup "
+#compress backup file
+backzip="$HOME/Documents/backups/logs_$datetime.zip"
+zip -r "$backzip" "$backupfile"
+#clear original logs
+sudo truncate -s 0 /var/log/syslog
+sudo truncate -s 0 /var/log/wtmp
+#read filesize of zipped logs
+filesize2=$(wc -c "$backzip" | awk '{print $1}')
+#display size comparision
+echo "File was reduced from $filesize bytes to $filesize2 bytes"
 
 # End
+
+
+# Not super happy with how this looks. I think if I had more time I could clean it up a bit, but it works
+
+# questions still:
+#           should I have the script delete the pre-zipped backup file?
+#           i couldn't get /dev/null to write into the files so i used truncate instead. it's the same effect but is it the same thing?
