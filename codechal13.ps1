69a32f7fda023e4a67770848a240745d61b6ab3b
CamBerry1 <cberry2008@gmail.com>
2023-06-03 18:24:44 -0700

    Clearned

--- codechal13.ps1
+++ codechal13.ps1
@@ -6,67 +6,78 @@
 #          - Have your script also create a new group in AD.
 #          - Have your script also create a new OU in AD.
 
+Import-Module activedirectory
 
+# define variables
+
+# Put into an array for scalability
+$deptlist = @("Finance", "Shipping", "TPS", "HR", "Sales", "Admin")
+$newuser = "y"
 
 # main
 
-Import-Module activedirectory
-
-# Put into an array for scalability
-$deptlist = @("Finance", "Shipping", "TPS")
-
 Write-Host "Set-up for new user"
-$firstname = read-host "New User first name"
-$lastname = read-host "New User last name"
+while ($newuser -eq "y") {
+    $firstname = read-host "New User first name"
+    $lastname = read-host "New User last name"
 
 # .substring arguments -> (starting index, length)
-$samaccountname = $firstname.Substring(0,1) + $lastname
+    $samaccountname = $firstname.Substring(0,1) + $lastname
 
 # lists departments and prompts for a choice
-foreach ($deptlist in $deptlist) {
-    write-host $deptlist
-    }
-$dept = read-host "Choose a department from the list above"
-if ($deptlist -notcontains $dept) {
-    Write-Host "Invalid Entry. Please restart script."
-    exit
-    }
+    foreach ($dept in $deptlist) {
+        write-host $dept
+        }
+    $dept = read-host "Choose a department from the list above"
+    if ($deptlist -notcontains $dept) {
+        Write-Host "Invalid Entry" 
+        exit                       
+        }
 
-$role = Read-Host "Enter new user's role"
+    $role = Read-Host "New user role"
 
-$mail = read-host "New user Email"
+    $mail = read-host "New user Email"
 
 # display information and ask for confirmation
-Write-Host $firstname $lastname; write-host $dept "Department"; write-host $role; write-host $mail
-$confirm = read-host "Is this information correct? [Y]"
+    Write-Host $firstname $lastname; write-host $dept "Department"; write-host $role; write-host $mail
+    $confirm = read-host "Is this information correct? [Y]"
 
 # create new user
-if ($confirm -eq "y") {
-    New-ADUser -SamAccountName $samaccountname -Name $firstname$lastname -EmailAddress $mail -OtherAttributes @{
-    'department'=$dept;
-    'title'=$role
-    }
-    Write-Host "New User Added Successfully"
-    }
-else {
-    Write-Host "Information not saved. Restart script to try again."
+    if ($confirm -eq "y") {
+        New-ADUser -SamAccountName $samaccountname -Name $firstname$lastname -EmailAddress $mail -OtherAttributes @{
+        'department'=$dept;
+        'title'=$role
+        }
+        Write-Host $samaccountname "Added Successfully"
+        $newuser = read-host "Add another user? [Y]"
+        }
+    else {
+        Write-Host "Information not saved. Restart script to try again."
+        exit
+        }
     }
 
 # optional create new OU
 $newOU = read-host "Create new OU? [Y]"
 if ($newOU -eq "y") {
-    $OUname = read-host "Please enter a name for the new OU"
-    New-ADOrganizationalUnit -Name $OUname -ProtectedFromAccidentalDeletion $false
-    write-host $OUname "OU created"
+    while ($newOU -eq "y") {
+        $OUname = read-host "Please enter a name for the new OU"
+        New-ADOrganizationalUnit -Name $OUname
+        write-host $OUname "OU created"
+        $newOU = Read-Host "Create another OU? [Y]"
+        }
     }
 
 # optional create new global security group
 # given more time, a menu system could be added to create different types of groups
 $newGroup = read-host "Create new Global Security Group? [Y]"
 if ($newGroup -eq "y") {
-    $Groupname = read-host "Please enter a name for the new Group"
-    New-ADGroup -Name $Groupname -GroupCategory Security -GroupScope Global
-    Write-Host $Groupname "group created"
+    while ($newGroup -eq "y") {
+        $Groupname = read-host "Please enter a name for the new Group"
+        New-ADGroup -Name $Groupname -GroupCategory Security -GroupScope Global
+        Write-Host $Groupname "group created"
+        $newGroup = Read-Host "Create another group? [Y]"
+        }
     }
 
 # end
